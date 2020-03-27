aa5d03ec2d18c0cabdd2f74d76e3a43fd9d164f4
Rufino Santiago <rsantiago@estrasol.com.mx>
2020-03-27 14:59:54 -0600

    seccion table

--- resources/views/front/purchase_information.blade.php
+++ resources/views/front/purchase_information.blade.php
@@ -33,12 +33,47 @@
 
 <div class="container-fluid purchase_header">
     <div class="container pt-4">
-        <div class="d-none d-md-block select_depart pl-1">
+        <div class="d-none d-md-block select_depart">
             <a href="{{ url('/')}}" class="rever_play" >
                 <i class="fa fa-chevron-left play" aria-hidden="true"></i>
             </a>
             &nbsp;&nbsp;&nbsp;Seleccionar otro departamento
         </div>
+    </div>
+</div>
+
+<div class="container areas">
+    <div class="row">
+
+        <div class="col-md-9">
+            <div class="row head_area mr-md-3 pt-2 pb-2">
+                <div class="col-md-3 border_area">
+                    <span class="title_area">ÁREA </span>
+                    <span class="subtitle_area">1</span>
+                </div>
+                <div class="col-md-9">
+                    <span class="title_area">Estílo </span>
+                    <span class="subtitle_area">Translúcida</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 pt-4 pb-3 column_total">
+            <div class="row pl-3 pr-3 border_inf">
+                <div class="col-6 text-subtotal pb-3">Subtotal.</div>
+                <div class="col-6 text-subtotal pb-3">$14,800.00</div>
+                <div class="col-6 text-subtotal pb-3">IVA.</div>
+                <div class="col-6 text-subtotal pb-3">$2,368.00</div>
+                <div class="col-6 text-subtotal pb-3">Total.</div>
+                <div class="col-6 text-total pb-4">$17,168.00</div>
+            </div>
+            <div class="text-center pt-3">
+                <button  class="btn-comprar pt-2 pb-2 ">
+                    COMPRAR
+                </button>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
