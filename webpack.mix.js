842644691406b2d37f32b1cacab6d39f1ff249bb
juan.camberos@estrasol.com.mx <juan.camberos@estrasol.com.mx>
2020-04-03 13:25:13 -0600

    Vista de Welcome con funcionalidad

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/purchase_information.scss', 'public/css')
     .sass('resources/sass/purchase_information_v2.scss', 'public/css')
+    .sass('resources/sass/signup.scss', 'public/css')
+    .sass('resources/sass/welcome.scss', 'public/css')
     .sass('resources/sass/app.scss', 'public/css');
